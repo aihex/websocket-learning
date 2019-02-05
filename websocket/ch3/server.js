7b7423f06851cfa8942424780f74f78bd7f6077c
aihex <heai0727@gmail.com>
2019-02-04 18:06:31 -0800

    [improve]differentiate notifincation

--- websocket/ch3/server.js
+++ websocket/ch3/server.js
@@ -1,12 +1,25 @@
 var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server,
-    // var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({
         port: 8181
     });
 var uuid = require('node-uuid');
 
 var clients = [];
+
+function wsSend(type, client_uuid, nickname, message) {
+    for (var i = 0; i < clients.length; i++) {
+        var clientSocket = clients[i].ws;
+        if (clientSocket.readyState === WebSocket.OPEN) {
+            clientSocket.send(JSON.stringify({
+                "type": type,
+                "id": client_uuid,
+                "nickname": nickname,
+                "message": message
+            }));
+        }
+    }
+}
 
 var clientIndex = 1;
 
@@ -20,43 +33,45 @@ wss.on('connection', function (ws) {
         "nickname": nickname
     });
     console.log('client [%s] connected', client_uuid);
+
+    var connect_message = nickname + " has connected";
+    wsSend("notification", client_uuid, nickname, connect_message);
+
     ws.on('message', function (message) {
-        if (message.indexOf('/nick') == 0) {
-            var nickname_array = message.split(' ')
+        if (message.indexOf('/nick') === 0) {
+            var nickname_array = message.split(' ');
             if (nickname_array.length >= 2) {
                 var old_nickname = nickname;
                 nickname = nickname_array[1];
-                for (var i = 0; i < clients.length; i++) {
-                    var clientSocket = clients[i].ws;
-                    var nickname_message = "Client " + old_nickname + " changed to " + nickname;
-                    clientSocket.send(JSON.stringify({
-                        "id": client_uuid,
-                        "nickname": nickname,
-                        "message": nickname_message
-                    }));
-                }
+                var nickname_message = "Client " + old_nickname + " changed to " + nickname;
+                wsSend("nick_update", client_uuid, nickname, nickname_message);
             }
         } else {
-            for (var i = 0; i < clients.length; i++) {
-                var clientSocket = clients[i].ws;
-                if (clientSocket.readyState === WebSocket.OPEN) {
-                    console.log('client [%s]: %s', clients[i].id, message);
-                    clientSocket.send(JSON.stringify({
-                        "id": client_uuid,
-                        "nickname": nickname,
-                        "message": message
-                    }));
-                }
-            }
+            wsSend("message", client_uuid, nickname, message);
         }
     });
 
-    ws.on('close', function () {
+    var closeSocket = function (customMessage) {
         for (var i = 0; i < clients.length; i++) {
             if (clients[i].id == client_uuid) {
-                console.log('client [%s] disconnected', client_uuid);
+                var disconnect_message;
+                if (customMessage) {
+                    disconnect_message = customMessage;
+                } else {
+                    disconnect_message = nickname + " has disconnected";
+                }
+                wsSend("notification", client_uuid, nickname, disconnect_message);
                 clients.splice(i, 1);
             }
         }
+    }
+    ws.on('close', function () {
+        closeSocket();
+    });
+
+    process.on('SIGINT', function () {
+        console.log("Closing things");
+        closeSocket('Server has disconnected');
+        process.exit();
     });
 });
